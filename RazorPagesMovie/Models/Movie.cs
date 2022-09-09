1c319c5a3ce7479b9270c3105fb0efa5450b59fc
Maland <maland83@gmail.com>
2022-09-09 20:33:01 +0300

    Init

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public String Genre { get; set; } = String.Empty;
+        public decimal Price { get; set; }
+
+    }
+}
